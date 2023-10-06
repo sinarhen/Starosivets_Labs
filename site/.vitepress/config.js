7aca04b4327ffc1abf949c7fda34c794904c91a2
Bogdan <105736826+sinarhen@users.noreply.github.com>
2023-10-06 13:20:57 +0300

    Update config.js

--- site/.vitepress/config.js
+++ site/.vitepress/config.js
@@ -17,7 +17,7 @@ export default defineConfig({
         items: [
                 { text: "Лабораторна робота №1", link: "/labs/1" },
                 { text: "Лабораторна робота №2", link: "/labs/2"},
-                { text: "Лабораторна робота №2", link: "/labs/3"} 
+                { text: "Лабораторна робота №3", link: "/labs/3"} 
                ],
       },  
     ],
