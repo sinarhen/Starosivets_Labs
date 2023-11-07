3b069c404add4eab335fdcf16f662322fce549f2
Bogdan <105736826+sinarhen@users.noreply.github.com>
2023-11-07 14:34:00 +0200

    Update filters.h

--- labs/filters.h
+++ labs/filters.h
@@ -1,22 +1,27 @@
 #include <iostream>
-#include <cctype>
-
-using namespace std;
-
-bool isNumeric(string value){
-    for (char c : value) {
-        if (isalpha(c) && c != '-' && c != '.') {
-            cout << "All characters must be digits" << endl;
-            return false;
-        }
-    }
-    return true;
-}
-
-bool isMoreThanOne(string value)
+# before lab_06
+// bool isNumeric(string value){
+//     for (char c : value) {
+//         if (isalpha(c) && c != '-' && c != '.') {
+//             cout << "All characters must be digits" << endl;
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// bool isMoreThanOne(string value)
+// {
+//     if (stoi(value) < 1 ){
+//         cout << "Value must be more than one" << endl;
+//         return false;
+//     };
+//     return true;
+// }
+# after lab_06
+bool isMoreThanOne(int val)
 {
-    if (stoi(value) < 1 ){
-        cout << "Value must be more than one" << endl;
+    if (val < 1 ){
+        std::cout << "Value must be more than one" << endl;
         return false;
     };
     return true;
