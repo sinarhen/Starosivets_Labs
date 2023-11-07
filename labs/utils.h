9715efb0a7cf7e875f6e443f12241c8f26fcb9a4
Bogdan <105736826+sinarhen@users.noreply.github.com>
2023-11-07 14:35:51 +0200

    Update utils.h

--- labs/utils.h
+++ labs/utils.h
@@ -1,20 +1,50 @@
+/* before lab_06 */
+
+// #include <iostream>
+
+// using namespace std;
+
+// template <class... FilterFuncs>
+// string getValue(FilterFuncs... funcs);
+
+// template <class... FilterFuncs>
+// string getValue(FilterFuncs... funcs) {
+//     string value;
+//     while (true) {
+//         cin >> value;
+//         if (!(... && funcs(value))) {
+//             cout << "Invalid input. Please enter a valid value." << endl;
+//         } else {
+//             break;
+//         }
+//     }
+//     return value;
+// }
+
+/* AFTER lab_06 */
+#ifndef UTILS_H
+#define UTILS_H
+
 #include <iostream>
 
 using namespace std;
 
-template <class... FilterFuncs>
-string getValue(FilterFuncs... funcs);
 
-template <class... FilterFuncs>
-string getValue(FilterFuncs... funcs) {
-    string value;
+template <typename T, class... FilterFuncs>
+T getValue(FilterFuncs... funcs) {
+    T value;
     while (true) {
         cin >> value;
         if (!(... && funcs(value))) {
             cout << "Invalid input. Please enter a valid value." << endl;
+        } else if (cin.fail()) {
+            cin.clear();
+            cin.ignore(256, '\n');
+            cout << "Invalid input. Please enter a valid value." << endl;
         } else {
-            break;
+            return value;
         }
     }
-    return value;
 }
+
+#endif // UTILS_H
